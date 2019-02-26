6c058ff946e558a5da21f3496f0ba7224bd61de2
yogo254 <yogo254@yahoo.com>
2019-02-26 04:53:26 +0300

    Updates

---
+++ precache-manifest.2929fdbdcb7a38da46a6d9324d92b541.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c254f0488665e2e6da3e",
+    "url": "/personalinfo/static/js/runtime~main.c254f048.js"
+  },
+  {
+    "revision": "c2f33f66cba216cb27cc",
+    "url": "/personalinfo/static/js/main.c2f33f66.chunk.js"
+  },
+  {
+    "revision": "a433137fc20575d9e49c",
+    "url": "/personalinfo/static/js/2.a433137f.chunk.js"
+  },
+  {
+    "revision": "c2f33f66cba216cb27cc",
+    "url": "/personalinfo/static/css/main.2460b135.chunk.css"
+  },
+  {
+    "revision": "61a4c4353e370383dbff427a67c18f3d",
+    "url": "/personalinfo/index.html"
+  }
+];
